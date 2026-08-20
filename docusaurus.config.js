26fe7a4ab1b5b8f5b48c5bcc58001bcfbc00b316
fikrimilano <fikrimilano1@gmail.com>
2026-08-20 19:19:48 +0700

    Title browser tabs with Open Health Stack

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "OHS Player Reference",
+  title: "Open Health Stack",
   tagline: "Evaluate, assemble, and adapt the OHS Player reference toolkit",
   favicon: "assets/favicon.svg",
   url: "https://ohs-foundation.github.io",
